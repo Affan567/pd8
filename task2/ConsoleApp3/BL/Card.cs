dfaba85082de2bc32ecd6c0afe80af0aca5e3d9b
m.affanasim515@gmail.com <m.affanasim515@gmail.com>
2025-04-13 14:58:00 +0500

    pd8

---
+++ task2/ConsoleApp3/BL/Card.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.BL
+{
+    internal class Card
+    {
+
+        private int value;
+        private int suit;
+
+        public Card(int value, int suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+
+        public int getvalue()
+        {
+            return value;
+        }
+
+        public int getsuit()
+        {
+            return suit;
+        }
+        public string getValueAsString()
+        {
+            if (value == 1)
+            {
+                return "Ace";
+            }
+            else if (value == 11)
+            {
+                return "Jack";
+            }
+            else if (value == 12)
+            {
+                return "Queen";
+            }
+            else if (value == 13)
+            {
+                return "King";
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+        public string getSuitAsString()
+        {
+            if (suit == 1)
+            {
+                return "Clubs";
+            }
+            else if (suit == 2)
+            {
+                return "Diamonds";
+            }
+            else if (suit == 3)
+            {
+                return "Spades";
+            }
+            else
+            {
+                return "Hearts";
+            }
+        }
+        public string tostring()
+        {
+            return getValueAsString() + "of" + getSuitAsString(); 
+        }
+    }
+}
---
+++ task2/ConsoleApp3/BL/Deck.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.BL
+{
+    internal class Deck
+    {
+
+            private int count;
+            private Card[] deck = new Card[52];
+
+            // Constructor: Initializes a full deck of 52 cards (13 values × 4 suits)
+            public Deck()
+            {
+                count = 0;
+                for (int x = 1; x <= 4; x++)
+                {       // Suits (1–4)
+                    for (int y = 1; y <= 13; y++)
+                    {   // Values (1–13)
+                        deck[count] = new Card(y, x); // Create card (value, suit)
+                        count++;
+                    }
+                }
+            }
+
+            // Shuffles the deck using the Fisher-Yates algorithm
+            public void shuffle()
+            {
+                System.Random rand = new System.Random();
+                Card temp;
+                for (int i = 0; i < 52; i++)
+                {
+                    int num = rand.Next(0, 52);  // Random index
+                    temp = deck[num];            // Swap cards
+                    deck[num] = deck[i];
+                    deck[i] = temp;
+                }
+                count = 52;  // Reset count after shuffling
+            }
+
+            // Deals the top card from the deck (returns null if empty)
+            public Card dealCard()
+            {
+                if (count > 0)
+                {
+                    count--;
+                    return deck[count];  // Return top card
+                }
+                else
+                {
+                    return null;          // Deck is empty
+                }
+            }
+
+            // Returns the number of cards left in the deck
+            public int cardsLeft()
+            {
+                return count;
+            }
+        }
+    }
